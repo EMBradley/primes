6a47116c2c6331d6c8f6aa74f7bf2b0aca97f181
Ethan Bradley <ethan.m.bradley@gmail.com>
2024-07-21 13:51:56 -0400

    use bigger wheel

--- src/lib.rs
+++ src/lib.rs
@@ -87,10 +87,14 @@ pub struct TrialDivision {
     lst: Vec<u64>,
 }
 
-const FIRST_PRIMES: [u64; 3] = [2, 3, 5];
-const WHEEL_LENGTH: usize = 8;
-const WHEEL_CIRCUMFERENCE: u64 = WHEEL_235[WHEEL_LENGTH - 1] + 1;
-const WHEEL_235: [u64; WHEEL_LENGTH] = [1, 7, 11, 13, 17, 19, 23, 29];
+const FIRST_PRIMES: [u64; 4] = [2, 3, 5, 7];
+const WHEEL_LENGTH: usize = 48;
+const WHEEL_CIRCUMFERENCE: u64 = WHEEL_2357[WHEEL_LENGTH - 1] + 1;
+const WHEEL_2357: [u64; WHEEL_LENGTH] = [
+    1, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101,
+    103, 107, 109, 113, 121, 127, 131, 137, 139, 143, 149, 151, 157, 163, 167, 169, 173, 179, 181,
+    187, 191, 193, 197, 199, 209,
+];
 
 #[derive(Debug, Clone, Copy)]
 struct Wheel {
@@ -156,7 +160,7 @@ impl Iterator for Wheel {
             self.index += 1;
         }
 
-        self.value = self.factor * (self.offset + WHEEL_235[self.index]);
+        self.value = self.factor * (self.offset + WHEEL_2357[self.index]);
         Some(self.value)
     }
 }
